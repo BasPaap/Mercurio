6726a48522635199478683ec3d16e7d1a68192e5
Bas Paap <baspaap@live.nl>
2020-11-28 17:02:05 +0100

    Enable dancing lights toggle

---
+++ Assets/Scripts/DancingLightsBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DancingLightsBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ControlSystemBehaviour.DancingLightsToggled += ControlSystemBehaviour_DancingLightsToggled;
+    }
+
+    private void ControlSystemBehaviour_DancingLightsToggled(object sender, bool e)
+    {
+        gameObject.SetActive(e);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
